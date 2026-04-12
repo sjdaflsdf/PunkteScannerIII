31411357a732854f85510ac4d68713618bbc2140
Rama <rama.alaboud@study.thws.de>
2026-04-12 20:47:24 +0200

    Projektstruktur angelegt: repository, service, dto packages

--- backend/src/main/java/com/example/demo/controller/ProduktController.java
+++ backend/src/main/java/com/example/demo/controller/ProduktController.java
@@ -3,17 +3,15 @@ package com.example.demo.controller;
 import com.example.demo.model.Produkt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("products")
 public class ProduktController {
 
-
     @GetMapping
-    public Produkt produkt(@PathParam("name") String name) {
+    public Produkt produkt(@RequestParam("name") String name) {
         return new Produkt(name, 3000);
     }
 }
